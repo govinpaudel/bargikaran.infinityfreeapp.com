75ef02be4a77540e0d7d601e9a8aaeaf2a56c1b0
Govinda paudel <govinpaudel@gmail.com>
2025-09-16 16:07:46 +0545

    2025-09-16

--- src/Actions/Action.jsx
+++ src/Actions/Action.jsx
@@ -29,13 +29,19 @@ export const getDetailsByKittaNo = async (office_id,napa_id,gabisa_id,ward_no,ki
   return res;
 };
 export const getDataByDate = async (date) => {
-  const res = await axios.get(`${API_URL}/getDataByDate/${date}`);
-  console.log(`${API_URL}/getDataByDate/${date}`);
+  const res = await axios.get(`http://10.7.33.8:5000/api/bargikaran/getDataByDate/${date}`);
+  console.log(`http://10.7.33.8:5000/api/bargikaran/getDataByDate/${date}`);
   return res;
 };
 
-export const sendRecordsToServer  = async (records) => {
-  const res = await axios.post(`${API_URL}/saveRecords`, records);  
-  console.log(`${API_URL}/saveRecords`,records);
+export const sendRecordsToServer = async (records) => {
+  // Send raw array instead of { records: [...] }
+  const res = await axios.post(`${API_URL}/saveRecords`, records, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  console.log("Sending to:", `${API_URL}/saveRecords`, records);
+  console.log("Response:", res.data);
   return res;
 };
